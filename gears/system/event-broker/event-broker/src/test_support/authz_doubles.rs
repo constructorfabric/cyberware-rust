constructorfabric/gears-rust

Implement event-broker dispatcher routing, REST handlers, and standalone single-process service (gears-rust#4345, #4346, #4347)
Wire ingest/delivery/dispatcher routing across all 19 REST endpoints with a
real ServiceDiscoveryV1-backed forward seam, and implement the handlers
themselves (ingest, producers, topics, event-types, consumer-groups,
subscriptions) with a full test harness and GTS-id-validated fixtures.
REST DTOs are consolidated into per-domain dto.rs files (ingest/delivery),
mirroring the house api/rest/dto.rs convention split across the two handler
groups this crate has.

Build the real, runnable standalone single-process deployment on top of
that: SpecificationManager's SQLite-backed startup cache (bulk-loaded from
types-registry), a Storage facade (SQLite for durable state, ClusterCacheV1
for the ephemeral subscription namespace), a real SQLite EventBrokerBackend
replacing the in-memory stub, a transactional ingest outbox
(toolkit-db's Outbox/LeasedMessageHandler) for atomic publish with
out-of-transaction backend persist, and a standalone [[bin]] with a
verified config (config/event-broker-standalone.yaml) that actually boots
and serves real REST traffic end to end.

Replace the delivery stream's fixed-interval poll with a real
ClusterCacheV1-backed wake-up (design.md D6): the ingest outbox notifies a
shared watch_prefix after every successful persist, and open :stream/:sse
connections wake on it instead of on a timer, verified by a test proving
the wake fires within 3s of a publish.

Add a full verification suite that boots the real binary rather than
mocking around it: a Rust integration-test module
(tests/standalone_integration_tests.rs) spawns the compiled
[[bin]] as a subprocess and drives it over real HTTP - publish/consume,
restart-durability (events and consumer groups survive a restart,
subscriptions correctly do not), producer-retry idempotency, and
consumer-group pagination - plus a Python e2e suite
(testing/e2e/gears/event_broker/) followi

gears/system/event-broker/event-broker/src/test_support/authz_doubles.rs
@@ -1,0 +1,63 @@
+//! Permissive-by-default `AuthZResolverClient` double for
+//! `EventBrokerHarness` (`gears-rust#4516`, `eb-authz-enforcement`).
+//! Always-allow so existing/new tests that don't care about authz keep
+//! passing unmodified; tests that DO care about denial construct their own
+//! narrower double instead of reusing this
+//! (design.md "Exact double shape ... is an implementation-time detail").
+//! Tenant scope is enforced via the same `AuthZResolverClient`/
+//! `PolicyEnforcer` seam (`domain/authz.rs::tenant_authorized`), not a
+//! separate `tenant-resolver-sdk` double - there is no second client type
+//! to fake here.
+
+use async_trait::async_trait;
+use authz_resolver_sdk::{
+    AuthZResolverClient, AuthZResolverError, EvaluationRequest, EvaluationResponse,
+    EvaluationResponseContext,
+};
+
+/// Grants every `evaluate` call - `IngestServiceImpl`/`DeliveryServiceImpl`'s
+/// `PolicyEnforcer::access_scope_with` calls only ever inspect
+/// `EvaluationResponse::decision`/`context.deny_reason` (`require_constraints(false)`
+/// means `context.constraints` is always empty regardless), so an
+/// unconditional allow with an empty context is a faithful "no PEP configured"
+/// stand-in.
+pub struct AllowAllAuthZ;
+
+#[async_trait]
+impl AuthZResolverClient for AllowAllAuthZ {
+    async fn evaluate(
+        &self,
+        _request: EvaluationRequest,
+    ) -> Result<EvaluationResponse, AuthZResolverError> {
+        Ok(EvaluationResponse {
+            decision: true,
+            context: EvaluationResponseContext::default(),
+        })
+    }
+}
+
+/// Denies `evaluate` whenever `deny_if` returns `true` for the request being
+/// checked, otherwise allows - lets a single test deny exactly the specific
+/// `event_type:produce`/`topic:consume`/`event_type:consume`/tenant-scope
+/// check it cares about (inspect `request.action.name` and
+/// `request.resource.properties`) while every other check on the same
+/// harness keeps its default allow.
+pub struct DenyingAuthZ<F> {
+    pub deny_if: F,
+}
+
+#[async_trait]
+impl<F> AuthZResolverClient for DenyingAuthZ<F>
+where
+    F: Fn(&EvaluationRequest) -> bool + Send + Sync,
+{
+    async fn evaluate(
+        &self,
+        request: EvaluationRequest,
+    ) -> Result<EvaluationResponse, AuthZResolverError> {
+        Ok(EvaluationResponse {
+            decision: !(self.deny_if)(&request),
+            context: EvaluationResponseContext::default(),
+        })
+    }
+}

constructorfabric/cyberware-rust

gears/system/event-broker/event-broker/src/test_support/authz_doubles.rs
@@ -1,0 +1,66 @@
+//! Permissive-by-default `AuthZResolverApi` double for
+//! `EventBrokerHarness` (`gears-rust#4516`, `eb-authz-enforcement`).
+//! Always-allow so existing/new tests that don't care about authz keep
+//! passing unmodified; tests that DO care about denial construct their own
+//! narrower double instead of reusing this
+//! (design.md "Exact double shape ... is an implementation-time detail").
+//! Tenant scope is enforced via the same `AuthZResolverApi`/
+//! `PolicyEnforcer` seam (`domain/authz.rs::tenant_authorized`), not a
+//! separate `tenant-resolver-sdk` double - there is no second client type
+//! to fake here.
+
+use async_trait::async_trait;
+use authz_resolver_sdk::{
+    AuthZResolverApi, EvaluationRequest, EvaluationResponse, EvaluationResponseContext,
+};
+use toolkit_canonical_errors::CanonicalError;
+use toolkit_security::PlatformSecurityContext;
+
+/// Grants every `evaluate` call - `IngestServiceImpl`/`DeliveryServiceImpl`'s
+/// `PolicyEnforcer::access_scope_with` calls only ever inspect
+/// `EvaluationResponse::decision`/`context.deny_reason` (`require_constraints(false)`
+/// means `context.constraints` is always empty regardless), so an
+/// unconditional allow with an empty context is a faithful "no PEP configured"
+/// stand-in.
+pub struct AllowAllAuthZ;
+
+#[async_trait]
+impl AuthZResolverApi for AllowAllAuthZ {
+    async fn evaluate(
+        &self,
+        _ctx: PlatformSecurityContext,
+        _request: EvaluationRequest,
+    ) -> Result<EvaluationResponse, CanonicalError> {
+        Ok(EvaluationResponse {
+            decision: true,
+            context: EvaluationResponseContext::default(),
+        })
+    }
+}
+
+/// Denies `evaluate` whenever `deny_if` returns `true` for the request being
+/// checked, otherwise allows - lets a single test deny exactly the specific
+/// `event_type:produce`/`topic:consume`/`event_type:consume`/tenant-scope
+/// check it cares about (inspect `request.action.name` and
+/// `request.resource.properties`) while every other check on the same
+/// harness keeps its default allow.
+pub struct DenyingAuthZ<F> {
+    pub deny_if: F,
+}
+
+#[async_trait]
+impl<F> AuthZResolverApi for DenyingAuthZ<F>
+where
+    F: Fn(&EvaluationRequest) -> bool + Send + Sync,
+{
+    async fn evaluate(
+        &self,
+        _ctx: PlatformSecurityContext,
+        request: EvaluationRequest,
+    ) -> Result<EvaluationResponse, CanonicalError> {
+        Ok(EvaluationResponse {
+            decision: !(self.deny_if)(&request),
+            context: EvaluationResponseContext::default(),
+        })
+    }
+}